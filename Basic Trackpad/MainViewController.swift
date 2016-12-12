85ff072cc75573f388c8c201b9b985dbfa5a0376
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2016-12-12 18:38:13 +0100

    Created project

---
+++ Basic Trackpad/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  Basic Trackpad
+//
+//  Created by Tomasz Pieczykolan on 12/12/16.
+//  Copyright © 2016 Tomasz Pieczykolan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
